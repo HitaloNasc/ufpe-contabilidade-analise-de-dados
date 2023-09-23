b04dc30945131702d42e2b4d4b4b957cd54f27d8
Hítalo Bruno <hitalo.bruno@salvus.me>
2023-09-23 00:26:18 -0300

    add table

---
+++ src/components/selector.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+export default function Selector(props) {
+  const { options, onChange, placeholder, value: selectedValue } = props;
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    console.log(value);
+    onChange(value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300, height: 38 }}>
+        <Select
+          displayEmpty
+          value={selectedValue}
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          renderValue={() => {
+            if (!selectedValue) {
+              return <em>{placeholder}</em>;
+            }
+            return selectedValue;
+          }}
+          MenuProps={MenuProps}
+          inputProps={{ "aria-label": "Without label" }}
+          sx={{ height: "100%" }}
+        >
+          <MenuItem disabled value="">
+            <em>{placeholder}</em>
+          </MenuItem>
+          {options.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
--- src/sections/overview/overview-latest-orders.js
+++ src/sections/overview/overview-latest-orders.js
@@ -1,104 +1,151 @@
-import { format } from 'date-fns';
-import PropTypes from 'prop-types';
-import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import {
   Box,
-  Button,
   Card,
-  CardActions,
-  CardHeader,
+  Typography,
   Divider,
-  SvgIcon,
   Table,
   TableBody,
   TableCell,
   TableHead,
-  TableRow
-} from '@mui/material';
-import { Scrollbar } from 'src/components/scrollbar';
-import { SeverityPill } from 'src/components/severity-pill';
-
-const statusMap = {
-  pending: 'warning',
-  delivered: 'success',
-  refunded: 'error'
-};
+  TableRow,
+  Button,
+} from "@mui/material";
+import { Scrollbar } from "src/components/scrollbar";
+import PlusSmallIcon from "@heroicons/react/24/solid/PlusSmallIcon";
+import anoOptions from "../../assets/options_ano.json";
+import estadoOptions from "../../assets/options_estado.json";
+import tipoOptions from "../../assets/options_tipo.json";
+import Selector from "../../components/selector";
 
 export const OverviewLatestOrders = (props) => {
-  const { orders = [], sx } = props;
+  const { data = [], sx } = props;
+  const initFilters = {
+    ano: "",
+    estado: "",
+    tipo: "",
+    categoria: "",
+    perPage: 25,
+  };
+  const [filters, setFilters] = useState(initFilters);
+
+  const applyFilters = (data, filters) => {
+    return data.filter((row) => {
+      if (filters.ano && row.ano !== filters.ano) {
+        return false;
+      }
+      if (filters.estado && row.estado !== filters.estado) {
+        return false;
+      }
+      if (filters.tipo && row.tipo !== filters.tipo) {
+        return false;
+      }
+      return true;
+    });
+  };
+
+  const filteredData = React.useMemo(() => {
+    return applyFilters(data, filters);
+  }, [data, filters]);
+
+  const handleFiltersChange = (filter) => (value) => {
+    setFilters((prevState) => ({ ...prevState, [filter]: value }));
+  };
+
+  const handleLoadMore = () => {
+    setFilters((prevState) => ({ ...prevState, perPage: prevState.perPage + 25 }));
+  };
+
+  const clearFilters = () => {
+    setFilters(initFilters);
+  };
+
+  const formatCurrency = (value) => {
+    if (value !== "-") {
+      const floatValue = parseFloat(value.replace(/\./g, "").replace(",", "."));
+      return floatValue.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    }
+    return value;
+  };
 
   return (
     <Card sx={sx}>
-      <CardHeader title="Latest Orders" />
+      <Typography variant="h3" mt={3} ml={1} mb={3}>
+        {"Tabela de despesas".toUpperCase()}
+      </Typography>
+      <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+        <Selector
+          placeholder="Ano"
+          options={anoOptions}
+          onChange={handleFiltersChange("ano")}
+          value={filters.ano}
+        />
+        <Selector
+          placeholder="Estado"
+          options={estadoOptions}
+          onChange={handleFiltersChange("estado")}
+          value={filters.estado}
+        />
+        <Selector
+          placeholder="Tipo de despesa"
+          options={tipoOptions}
+          onChange={handleFiltersChange("tipo")}
+          value={filters.tipo}
+        />
+        <Button variant="text" onClick={clearFilters} sx={{ height: 38, m: 1 }} color="inherit">
+          Limpar
+        </Button>
+      </Box>
       <Scrollbar sx={{ flexGrow: 1 }}>
-        <Box sx={{ minWidth: 800 }}>
+        <Box sx={{ minWidth: 800, width: "100%" }}>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>
-                  Order
-                </TableCell>
-                <TableCell>
-                  Customer
-                </TableCell>
-                <TableCell sortDirection="desc">
-                  Date
-                </TableCell>
-                <TableCell>
-                  Status
-                </TableCell>
+                <TableCell>Ano</TableCell>
+                <TableCell>Instituição</TableCell>
+                <TableCell>Estado</TableCell>
+                <TableCell>População</TableCell>
+                <TableCell>Tipo</TableCell>
+                <TableCell>Categoria</TableCell>
+                <TableCell>Valor</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {orders.map((order) => {
-                const createdAt = format(order.createdAt, 'dd/MM/yyyy');
-
+              {filteredData.slice(0, filters.perPage).map((row, index) => {
+                const categoria = `${row.cod_conta ?? ""} ${row.nome_conta ?? ""}`;
                 return (
-                  <TableRow
-                    hover
-                    key={order.id}
-                  >
-                    <TableCell>
-                      {order.ref}
-                    </TableCell>
-                    <TableCell>
-                      {order.customer.name}
-                    </TableCell>
-                    <TableCell>
-                      {createdAt}
-                    </TableCell>
-                    <TableCell>
-                      <SeverityPill color={statusMap[order.status]}>
-                        {order.status}
-                      </SeverityPill>
-                    </TableCell>
+                  <TableRow hover key={index}>
+                    <TableCell>{row.ano}</TableCell>
+                    <TableCell>{row.instituicao}</TableCell>
+                    <TableCell>{row.estado}</TableCell>
+                    <TableCell>{row.populacao_total}</TableCell>
+                    <TableCell>{row.tipo}</TableCell>
+                    <TableCell>{categoria}</TableCell>
+                    <TableCell>{formatCurrency(String(row.valor))}</TableCell>
                   </TableRow>
                 );
               })}
             </TableBody>
           </Table>
+          <Box sx={{ textAlign: "center", margin: 2 }}>
+            {filters.perPage < filteredData.length && (
+              <Button variant="contained" color="primary" onClick={handleLoadMore}>
+                <PlusSmallIcon />
+              </Button>
+            )}
+          </Box>
         </Box>
       </Scrollbar>
       <Divider />
-      <CardActions sx={{ justifyContent: 'flex-end' }}>
-        <Button
-          color="inherit"
-          endIcon={(
-            <SvgIcon fontSize="small">
-              <ArrowRightIcon />
-            </SvgIcon>
-          )}
-          size="small"
-          variant="text"
-        >
-          View all
-        </Button>
-      </CardActions>
     </Card>
   );
 };
 
-OverviewLatestOrders.prototype = {
-  orders: PropTypes.array,
-  sx: PropTypes.object
+OverviewLatestOrders.propTypes = {
+  data: PropTypes.array,
+  sx: PropTypes.object,
 };
